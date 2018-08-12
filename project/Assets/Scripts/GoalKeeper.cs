97c18eb31182f7758537336c390c1c51620768b6
Binnnn <925014996@qq.com>
2018-08-12 22:58:19 +0800

    门将扑救
    门将判断方向往指定方向扑球

--- project/Assets/Scripts/GoalKeeper.cs
+++ project/Assets/Scripts/GoalKeeper.cs
@@ -6,43 +6,74 @@ public class GoalKeeper : MonoBehaviour {
     public GameObject goalKeeper;
     private Animator goalKeeperAnimator;
     private Rigidbody goalKeeperRgd;
-    public float smoothing = 20f;
+    private Vector3 targetPos;
     private void Start()
     {
         goalKeeperAnimator = goalKeeper.GetComponent<Animator>();
         goalKeeperRgd = goalKeeper.GetComponent<Rigidbody>();
     }
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Ball" && transform.parent.name == "GoalKeeper_Opponent")
+        if (other.tag == "Ball" && transform.name == "OpponentTrigger")
         {
             if(other.transform.position.x - transform.position.x > 0)
             {
-                if(other.transform.position.y<2)
+                if (other.transform.position.y < 1)
+                {
                     goalKeeperAnimator.SetTrigger("Right_down");
+                    targetPos = new Vector3(3, goalKeeperRgd.position.y, goalKeeperRgd.position.z);
+                }
                 else
+                {
                     goalKeeperAnimator.SetTrigger("Right_Up");
+                    targetPos = new Vector3(3, 2, goalKeeperRgd.position.z);
+                }                    
             }
             else if(other.transform.position.x - transform.position.x < 0)
             {
-                if (other.transform.position.y < 2)
+                if (other.transform.position.y < 1)
+                {
                     goalKeeperAnimator.SetTrigger("Left_down");
+                    targetPos = new Vector3(-3, goalKeeperRgd.position.y, goalKeeperRgd.position.z);
+                }
                 else
+                {
                     goalKeeperAnimator.SetTrigger("Left_Up");
+                    targetPos = new Vector3(-3, 2, goalKeeperRgd.position.z);
+                }                   
             }
-            goalKeeperRgd.MovePosition(Vector3.Lerp(goalKeeperRgd.position, other.transform.position,smoothing* Time.deltaTime));
+            goalKeeperRgd.MovePosition(Vector3.Lerp(goalKeeperRgd.position, targetPos, 0.5f));
+            Debug.Log(goalKeeperRgd.position);
         }
-        if (other.tag == "Ball" && transform.parent.name == "GoalKeeper_Team1")
+        if (other.tag == "Ball" && transform.name == "Team1Trigger")
         {
             if (other.transform.position.x - transform.position.x < 0)
             {
-                goalKeeperAnimator.SetTrigger("Right_down");
+                if (other.transform.position.y < 1)
+                {
+                    goalKeeperAnimator.SetTrigger("Right_down");
+                    targetPos = new Vector3(-3, goalKeeperRgd.position.y, goalKeeperRgd.position.z);
+                }
+                else
+                {
+                    goalKeeperAnimator.SetTrigger("Right_Up");
+                    targetPos = new Vector3(-3, 2, goalKeeperRgd.position.z);
+                }
             }
             else if (other.transform.position.x - transform.position.x > 0)
             {
-                goalKeeperAnimator.SetTrigger("Left_down");
+                if (other.transform.position.y < 1)
+                {
+                    goalKeeperAnimator.SetTrigger("Left_down");
+                    targetPos = new Vector3(3, goalKeeperRgd.position.y, goalKeeperRgd.position.z);
+                }
+                else
+                {
+                    goalKeeperAnimator.SetTrigger("Left_Up");
+                    targetPos = new Vector3(3, 2, goalKeeperRgd.position.z);
+                }
             }
-
+            goalKeeperRgd.MovePosition(Vector3.Lerp(goalKeeperRgd.position, targetPos, 0.5f));
         }
     }
 }
